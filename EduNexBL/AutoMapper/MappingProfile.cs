c213dce437756ddd0563e2744dd9a6cf064a1c0a
Ahmed Amin <ahmamin11@gmail.com>
2024-05-13 02:32:53 +0300

    add course type

--- EduNexBL/AutoMapper/MappingProfile.cs
+++ EduNexBL/AutoMapper/MappingProfile.cs
@@ -1,4 +1,5 @@
-﻿using AutoMapper;
+﻿using Amazon.S3.Model;
+using AutoMapper;
 using EduNexBL.DTOs;
 using EduNexBL.DTOs.AuthDtos;
 using EduNexBL.DTOs.CourseDTOs;
@@ -53,8 +54,9 @@ namespace EduNexBL.AutoMapper
                .ForMember(dest => dest.LevelName, opt => opt.MapFrom(src => src.Subject.Level.LevelName))
                .ForMember(dest => dest.SubjectName, opt => opt.MapFrom(src => src.Subject.SubjectName))
                .ForMember(dest => dest.ProfilePhoto, opt => opt.MapFrom(src => src.Teacher.ProfilePhoto))
-               .ForMember(dest => dest.TeacherName, opt => opt.MapFrom(src => src.Teacher.FirstName + ' ' + src.Teacher.LastName));
-        }
+               .ForMember(dest => dest.TeacherName, opt => opt.MapFrom(src => src.Teacher.FirstName + ' ' + src.Teacher.LastName))
+               .ForMember(dest => dest.CourseType, opt => opt.MapFrom(src => src.Subject.SubjectType));
+        }      
 
         private static int CalculateAge(DateTime dateOfBirth)
         {
