d1e3a0b982c3e3a64e17f85b8f92facd4201038d
MostafaSalem <103802177+moostafasal@users.noreply.github.com>
2024-05-13 02:51:31 +0200

    add

---
+++ EDuNexDB/Migrations/20240513004958_addtype.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EduNexDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class addtype : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
