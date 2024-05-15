0ac2a5eacf57e65e1a562f56958654799cdc28d9
PhilopatirAbdallah <144623020+PhilopatirAbdallah@users.noreply.github.com>
2024-05-15 23:23:32 +0300

    add counters

--- EduNexAPI/Controllers/HomeController.cs
+++ EduNexAPI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using EduNexBL.UnitOfWork;
+﻿using EduNexBL.IRepository;
+using EduNexBL.UnitOfWork;
 using EduNexDB.Context;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,10 +11,14 @@ namespace EduNexAPI.Controllers
     public class HomeController : ControllerBase
     {
         private readonly IUnitOfWork _unitOfWork;
+        private readonly EduNexContext _Context;
+        private readonly IAdminRepository _adminRepository;
 
-        public HomeController(IUnitOfWork unitOfWork)
+        public HomeController(IUnitOfWork unitOfWork, EduNexContext context, IAdminRepository adminRepository)
         {
             _unitOfWork = unitOfWork;
+            _Context = context;
+            _adminRepository = adminRepository;
         }
 
         [HttpGet("GetStudentsOrderedByScore")]
@@ -30,5 +35,92 @@ namespace EduNexAPI.Controllers
             }
         }
 
+        [HttpPost("GetCoursesCount")]
+        public async Task<IActionResult> GetCoursesCount()
+        {
+            try
+            {
+                var Courses = await _unitOfWork.CourseRepo.GetAll();
+                int CoursesCount = Courses.Count();
+                if (CoursesCount > 0)
+                {
+                    return Ok(CoursesCount);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpPost("GetStudentCount")]
+        public async Task<IActionResult> GetStudentCount()
+        {
+            try
+            {
+                var Students = await _unitOfWork.StudentRepo.GetAll();
+                int StudentsCount = Students.Count();
+                if (StudentsCount > 0)
+                {
+                    return Ok(StudentsCount);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        
+        [HttpPost("GetTeachersCount")]
+        public async Task<IActionResult> GetTeachersCount()
+        {
+            try
+            {
+                var Teachers = await _adminRepository.GetTeachersAsync();
+                int TeachersCount = Teachers.Count();
+                if (TeachersCount > 0)
+                {
+                    return Ok(TeachersCount);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+        
+        [HttpPost("GetLecturesCount")]
+        public async Task<IActionResult> GetLecturesCount()
+        {
+            try
+            {
+                var Lectures = await _unitOfWork.LectureRepo.GetAll();
+                int LecturesCount = Lectures.Count();
+                if (LecturesCount > 0)
+                {
+                    return Ok(LecturesCount);
+                }
+                else
+                {
+                    return NotFound();
+                }
+            }
+            catch(Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
     }
 }
